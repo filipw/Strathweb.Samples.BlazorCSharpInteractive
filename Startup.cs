aa691a704d293c6304050490290b29896badd84f
filipw <wojcieszyn@gmail.com>
2019-05-01 00:54:26 +0200

    stuff

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Strathweb.Samples.BlazorCSharpInteractive
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
